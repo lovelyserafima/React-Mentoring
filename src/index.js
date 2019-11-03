59b6a6877182c6ca823e0dd162da52a2f2361701
Artur Liup <artur_liup@epam.com>
2019-11-03 19:55:22 +0300

    modified to arrow function, modified to jsx

--- src/index.js
+++ src/index.js
@@ -8,4 +8,4 @@ import FunctionalComponent from "./components/functionalComponent";
 ReactDOM.render(element, document.getElementById('React.createElement'));
 ReactDOM.render(<ReactComponent/>, document.getElementById('React.Component'));
 ReactDOM.render(<ReactPureComponent/>, document.getElementById('React.PureComponent'));
-ReactDOM.render(FunctionalComponent, document.getElementById("Functional component"));
+ReactDOM.render(<FunctionalComponent/>, document.getElementById("Functional component"));
