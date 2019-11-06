f71fc0ad9152eeb46b1720aa544e547511d2f872
Artur Liup <artur_liup@epam.com>
2019-11-06 12:55:31 +0300

    minimizer need an array

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,7 +5,7 @@ const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(common, {
     optimization: {
-        minimizer: new OptimizeCSSAssetsPlugin({}),
+        minimizer: [ new OptimizeCSSAssetsPlugin({}) ]
     },
     plugins: [
         new MiniCssExtractPlugin({
